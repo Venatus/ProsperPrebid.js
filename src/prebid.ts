c50f1dd24d027aa1d93df7c63a6a31e20261e6a4
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2023-04-25 10:59:36 +0200

    expose registerBidder publicly
    c18c7142dd478edc1563558c0172919ad8b41d5a
    
    # Conflicts:
    #	src/prebid.js
    
    # Conflicts:
    #	src/prebid.js
    
    # Conflicts:
    #	src/prebid.js
    
    # Conflicts:
    #	src/prebid.js
    
    # Conflicts:
    #	src/prebid.js
    
    # Conflicts:
    #	src/prebid.js
    
    # Conflicts:
    #	src/prebid.js
    
    # Conflicts:
    #	src/prebid.ts
    
    # Conflicts:
    #	src/prebid.ts

--- src/prebid.ts
+++ src/prebid.ts
@@ -63,6 +63,8 @@ import type { AnyFunction, Wraps } from "./types/functions.d.ts";
 import type { BidderScopedSettings, BidderSettings } from "./bidderSettings.ts";
 import { fillAudioDefaults, ORTB_AUDIO_PARAMS } from './audio.ts';
 
+import {registerBidder} from './adapters/bidderFactory.js';
+
 import { getGlobalVarName } from "./buildOptions.ts";
 import { yieldAll } from "./utils/yield.ts";
 
@@ -482,8 +484,13 @@ declare module './prebidGlobal' {
     processQueue: typeof processQueue;
     triggerBilling: typeof triggerBilling;
     refreshPageViewId: typeof refreshPageViewId;
+
+    registerBidder: typeof registerBidder;
   }
 }
+
+/** register a bidder */
+addApiMethod('registerBidder', registerBidder);
 
 // Allow publishers who enable user sync override to trigger their sync
 addApiMethod('triggerUserSyncs', triggerUserSyncs);

7f083984fdec7ad62ed7da9d526dd7c4077678c1
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2023-05-08 11:18:01 +0200

    support async loading
    # Conflicts:
    #	src/prebidGlobal.js

--- src/prebidGlobal.ts
+++ src/prebidGlobal.ts
@@ -44,7 +44,11 @@ export function getGlobal() {
 }
 
 export function registerModule(name: string) {
-  if (!global.installedModules.includes(name)) {
-    global.installedModules.push(name);
+  if (!global.installedModules) {
+    global.que.push(() => registerModule(name));
+  } else {
+    if (!global.installedModules.includes(name)) {
+      global.installedModules.push(name);
+    }
   }
 }

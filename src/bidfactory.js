f0f77f78aa4383f2522f7d2a06d90cc9e6462fb3
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2023-08-03 14:22:05 +0200

    basic support to load (external) bids
    326afed1d39ca4e3cce002ff3d24023177f32a1d
    
    # Conflicts:
    #	src/auctionManager.js
    
    # Conflicts:
    #	src/auctionManager.js

--- src/bidfactory.js
+++ src/bidfactory.js
@@ -70,3 +70,36 @@ function Bid(statusCode, {src = 'client', bidder = '', bidId, transactionId, adU
 export function createBid(statusCode, identifiers) {
   return new Bid(statusCode, identifiers);
 }
+
+export function restoreValidBid(origBid) {
+  const bid = createBid(1, origBid);
+  const {adId, adapterCode, adResponse, adUnitCode, bidder, bidderCode, width, height, ad, mediaType, renderer, requestTimestamp, ttl, cpm, originalCPM, originalCurrency, ...rest} = origBid;
+  try{
+    /*if(bid.bidderCode && bid.bidderCode.indexOf('debugger')==-1){
+      debugger;
+    }else*/ if(!bid.bidderCode){
+      debugger;
+    }
+  }catch(e){
+    debugger;
+  }
+  if (rest) {
+    
+  }
+  bid.adId = adId;
+  bid.adapterCode = adapterCode;
+  bid.adResponse = adResponse;
+  bid.adUnitCode = adUnitCode;
+  bid.bidder = bidder;
+  bid.bidderCode = bidderCode;
+  bid.ad = ad;
+  bid.cpm = cpm;
+  bid.mediaType = mediaType;
+  bid.originalCPM = originalCPM;
+  bid.width = width;
+  bid.height = height;
+  bid.requestTimestamp = requestTimestamp;
+  bid.renderer = renderer;
+  bid.ttl = ttl;
+  return bid;
+}

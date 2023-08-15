6f79643a964d68e1eb36bd97088b77b512987a61
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2023-08-15 09:56:54 +0200

    allow instream video to skip cache service

--- src/video.js
+++ src/video.js
@@ -118,7 +118,7 @@ export function isValidVideoBid(bid, {index = auctionManager.index} = {}) {
 }
 
 export const checkVideoBidSetup = hook('sync', function(bid, adUnit, videoMediaType, context, useCacheKey) {
-  if (videoMediaType && (useCacheKey || context !== OUTSTREAM)) {
+  if (videoMediaType && (useCacheKey || typeof(useCacheKey) === 'undefined'/*context !== OUTSTREAM*/)) {//allow instream video to not use the cache service
     // xml-only video bids require a prebid cache url
     const { url, useLocal } = config.getConfig('cache') || {};
     if ((!url && !useLocal) && bid.vastXml && !bid.vastUrl) {

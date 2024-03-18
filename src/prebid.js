8c662a5724c276b65ea82bea18d3a2972d65e6f4
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2024-03-18 16:45:23 +0100

    allow setting alternative fetch method

--- src/prebid.js
+++ src/prebid.js
@@ -43,6 +43,7 @@ import {renderAdDirect} from './adRendering.js';
 import {getHighestCpm} from './utils/reducers.js';
 import {fillVideoDefaults} from './video.js';
 import {registerBidder} from './adapters/bidderFactory.js';
+import { dep } from './ajax.js';
 
 const pbjsInstance = getGlobal();
 const { triggerUserSyncs } = userSync;
@@ -1005,5 +1006,9 @@ pbjsInstance.triggerBilling = (winningBid) => {
     logWarn('The bid provided to triggerBilling did not match any bids received.');
   }
 };
+
+pbjsInstance.setFetchMethod = (method) => {
+  dep.fetch = method;
+}
 
 export default pbjsInstance;

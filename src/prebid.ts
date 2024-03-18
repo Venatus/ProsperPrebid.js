6bb35d822754f9c121d9b9beb7bfae0f2b3026a1
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2024-03-18 16:45:23 +0100

    allow setting alternative fetch method
    # Conflicts:
    #	src/prebid.js
    
    # Conflicts:
    #	src/prebid.ts
    
    # Conflicts:
    #	src/prebid.ts

--- src/prebid.ts
+++ src/prebid.ts
@@ -63,6 +63,7 @@ import type {AnyFunction, Wraps} from "./types/functions.d.ts";
 import type {BidderScopedSettings, BidderSettings} from "./bidderSettings.ts";
 import {fillAudioDefaults, ORTB_AUDIO_PARAMS} from './audio.ts';
 import {registerBidder} from './adapters/bidderFactory.js';
+import { dep } from './ajax.js';
 
 import {getGlobalVarName} from "./buildOptions.ts";
 import {yieldAll} from "./utils/yield.ts";
@@ -487,6 +488,7 @@ declare module './prebidGlobal' {
     registerBidder: typeof registerBidder;
     addBids: typeof addBids;
     getAllReceivedBids: typeof auctionManager.getAllReceivedBids;
+    setFetchMethod: (method: (url: string, options?: RequestInit) => Promise<Response>) => void;
   }
 }
 
@@ -1313,5 +1315,9 @@ function refreshPageViewId() {
   }
 }
 addApiMethod('refreshPageViewId', refreshPageViewId);
+
+pbjsInstance.setFetchMethod = (method) => {
+  dep.fetch = method;
+}
 
 export default pbjsInstance;

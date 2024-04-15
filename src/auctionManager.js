acf42e0124907724c92dd0aed49c0c2f770f417d
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2024-04-15 17:52:15 +0200

    basic restoreBids as reference (note, the methods needs to be implemented manually in this case)

--- src/auctionManager.js
+++ src/auctionManager.js
@@ -164,7 +164,7 @@ export function newAuctionManager() {
     _auctions.clear();
   }
 
-  auctionManager.addBids = function(bids, adunit) {
+  auctionManager.addBids = function(bids, adunit, asReference=false) {
     if (!store) {
       store = this.createAuction({
         adUnits: [],
@@ -172,7 +172,7 @@ export function newAuctionManager() {
       });
     }
 
-    const bidsCopy = bids.map(bid => {
+    const bidsCopy = asReference ? bids : bids.map(bid => {
       return restoreValidBid(bid);
     });
 

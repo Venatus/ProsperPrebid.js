3866c30b5e6ede057491307c8372815fbad218f5
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2024-10-25 12:13:56 +0200

    added event for invalid bid requests

--- src/adapters/bidderFactory.ts
+++ src/adapters/bidderFactory.ts
@@ -376,6 +376,7 @@ export function newBidder<B extends BidderCode>(spec: BidderSpec<B>) {
   function filterAndWarn(bid) {
     if (!spec.isBidRequestValid(bid)) {
       logWarn(`Invalid bid sent to bidder ${spec.code}: ${JSON.stringify(bid)}`);
+      events.emit(EVENTS.BIDDER_INVALID_REQUEST, bid);
       return false;
     }
     return true;

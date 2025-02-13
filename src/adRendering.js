69dcbb1fb26c26608a6666be97111774218509dd
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2025-02-13 16:10:58 +0100

    allow to overwrite the bid-sizes used to set the iframe's dimensions

--- src/adRendering.js
+++ src/adRendering.js
@@ -127,7 +127,7 @@ function creativeMessageHandler(deps) {
 }
 
 export const getRenderingData = hook('sync', function (bidResponse, options) {
-  const {ad, adUrl, cpm, originalCpm, width, height} = bidResponse
+  const {ad, adUrl, cpm, originalCpm, width, height, adjWidth, adjHeight} = bidResponse
   const repl = {
     AUCTION_PRICE: originalCpm || cpm,
     CLICKTHROUGH: options?.clickUrl || ''
@@ -135,8 +135,8 @@ export const getRenderingData = hook('sync', function (bidResponse, options) {
   return {
     ad: replaceMacros(ad, repl),
     adUrl: replaceMacros(adUrl, repl),
-    width,
-    height,
+    width: adjWidth ?? width,
+    height: adjHeight ?? height,
     setDimensionsAsStyle:options?.setDimensionsAsStyle,
   };
 })

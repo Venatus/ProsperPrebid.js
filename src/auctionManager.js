4b5c4a6c303c01d8fce60d391d4bf1acd5f40264
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2025-07-05 22:34:11 +0200

    Squashed commit of the following: auctionManager - external bids
    commit 564c82fad2235376416dbec6b20102d8ec256507
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Mon Dec 23 18:02:26 2024 +0100
    
        add bids as item
    
    commit e2767f9bca8584232a910658d7cac7ae5b10c884
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Mon Nov 11 18:55:13 2024 +0100
    
        allow access to all received bids
    
        # Conflicts:
        #	src/prebid.js
    
        # Conflicts:
        #	src/prebid.ts
    
    commit 7e442e9bb3b930c87d8d4905ec9bd8e3414ca9e4
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Mon Apr 15 17:52:15 2024 +0200
    
        basic restoreBids as reference (note, the methods needs to be implemented manually in this case)
    
        # Conflicts:
        #	src/prebid.js
    
    commit d9d3220839816db05633d279f47c785e13f968f0
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Tue Mar 19 15:52:50 2024 +0100
    
        getAuction by Bid instance
    
        # Conflicts:
        #	src/auctionManager.js
    
    commit 18b1ed1c44d85a521ae09a1a24a50e73aaaceb4c
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Thu Aug 3 14:22:05 2023 +0200
    
        basic support to load (external) bids
    
        326afed1d39ca4e3cce002ff3d24023177f32a1d
    
        # Conflicts:
        #	src/auctionManager.js
    
        # Conflicts:
        #	src/auctionManager.js
    
        # Conflicts:
        #	src/auctionManager.js
    
        # Conflicts:
        #	src/adapterManager.js
        #	src/bidfactory.js
        #	src/prebid.js
    
    # Conflicts:
    #	src/adapterManager.ts
    #	src/prebid.ts
    
    # Conflicts:
    #	src/auctionManager.js
    
    # Conflicts:
    #	src/auctionManager.js

--- src/auctionManager.js
+++ src/auctionManager.js
@@ -23,15 +23,19 @@
  * @property {function(): void} clearAllAuctions - clear all auctions for testing
  * @property {function(*): *} onExpiry
  * @property {AuctionIndex} index
+ * @property {function(Array, string, boolean): void} addBids - add bids to an auction
+ * @property {function(): Array} getAllReceivedBids - get all received auctions
  */
 
 import { uniques, logWarn } from './utils.js';
 import { newAuction, getStandardBidderSettings, AUCTION_COMPLETED } from './auction.js';
 import { AuctionIndex } from './auctionIndex.js';
+import { restoreValidBid } from './bidfactory.js';
 import { BID_STATUS, JSON_MAPPING } from './constants.js';
 import { useMetrics } from './utils/perfMetrics.js';
 import { ttlCollection } from './utils/ttlCollection.js';
 import { getEffectiveMinBidCacheTTL, getMinBidCacheTTL, onMinBidCacheTTLChange } from './bidTTL.js';
+import adapterManager from './adapterManager';
 
 /**
  * Creates new instance of auctionManager. There will only be one instance of auctionManager but
@@ -63,10 +67,17 @@ export function newAuctionManager() {
   const auctionManager = {
     onExpiry: _auctions.onExpiry
   };
+  let store = null;
 
   function getAuction(auctionId) {
     for (const auction of _auctions) {
       if (auction.getAuctionId() === auctionId) return auction;
+    }
+  }
+
+  function getAuctionByBid(bid) {
+    for (const auction of _auctions.toArray()) {
+      if (auction.getBidsReceived().find(b=>b.adId === bid.adId && b.adUnitCode === bid.adUnitCode && b.bidderCode === bid.bidderCode && b.auctionId === bid.auctionId)) return auction;
     }
   }
 
@@ -75,7 +86,7 @@ export function newAuctionManager() {
     metrics.checkpoint('bidWon');
     metrics.timeBetween('auctionEnd', 'bidWon', 'adserver.pending');
     metrics.timeBetween('requestBids', 'bidWon', 'adserver.e2e');
-    const auction = getAuction(bid.auctionId);
+    const auction = getAuction(bid.auctionId) ?? getAuctionByBid(bid);
     if (auction) {
       auction.addWinningBid(bid);
     } else {
@@ -115,6 +126,10 @@ export function newAuctionManager() {
   function allBidsReceived() {
     return _auctions.toArray().flatMap(au => au.getBidsReceived())
   }
+
+  auctionManager.getAllReceivedBids = function() {
+    return allBidsReceived();
+  };
 
   auctionManager.getAllBidsForAdUnitCode = function(adUnitCode) {
     return allBidsReceived()
@@ -156,6 +171,36 @@ export function newAuctionManager() {
 
   auctionManager.clearAllAuctions = function() {
     _auctions.clear();
+  }
+
+  auctionManager.addBids = function(bids, adunit, asReference=false) {
+    if (!store) {
+      store = this.createAuction({
+        adUnits: [],
+        adUnitCodes: []
+      });
+    }
+
+    const bidsCopy = asReference ? bids : bids.map(bid => {
+      return restoreValidBid(bid);
+    });
+
+    bidsCopy.forEach((bid) =>
+    {
+      adapterManager.callRestoreBidRenderer(bid, adunit);
+    });
+
+    const bidIds = {};
+    bidsCopy.forEach(bidCopy => {
+      if (bidIds[bidCopy.adId]) {
+        logWarn(`Duplicate adId ignored: ${bidCopy.adId}`);
+      } else {
+        bidIds[bidCopy.adId] = true;
+        store.addBidReceived(bidCopy);
+      }
+    });
+
+    return store;
   }
 
   function _addAuction(auction) {

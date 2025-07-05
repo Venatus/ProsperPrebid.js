9565b7eca6fccb6ccb6c83bb5b0079433727613d
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2025-07-05 21:30:54 +0200

    Squashed commit of the following: additional bidder-adapter responses/events
    commit 7e6af39a0a9171255eeea991d2a697958a279295
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Fri Oct 25 12:13:56 2024 +0200
    
        added event for invalid bid requests
    
    commit 25629136ad99657d4b722c85a9bf6afae4117207
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Sat Jul 5 19:56:09 2025 +0200
    
        fix typing
    
    commit ea48d8552c2c239092cc6de17469a236d4730dfb
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Wed Apr 5 11:02:01 2023 +0200
    
        added DIRECT and PROMISE request methods
    
    # Conflicts:
    #	src/adapters/bidderFactory.ts

--- src/adapters/bidderFactory.ts
+++ src/adapters/bidderFactory.ts
@@ -104,8 +104,12 @@ const TIDS = {
 export interface AdapterRequest {
   url: string;
   data: any;
-  method?: 'GET' | 'POST';
+  method?: 'GET' | 'POST' | 'DIRECT' | 'PROMISE';
   options?: Omit<AjaxOptions, 'method'> & { endpointCompression?: boolean };
+    response?: {
+      responseText?: string;
+    };
+    promise?: Promise<{responseText: string, getResponseHeader: (hdr:any)=>any}>;
 }
 
 export interface ServerResponse {
@@ -381,6 +385,7 @@ export function newBidder<B extends BidderCode>(spec: BidderSpec<B>) {
   function filterAndWarn(bid) {
     if (!spec.isBidRequestValid(bid)) {
       logWarn(`Invalid bid sent to bidder ${spec.code}: ${JSON.stringify(bid)}`);
+      events.emit(EVENTS.BIDDER_INVALID_REQUEST, bid);
       return false;
     }
     return true;
@@ -573,6 +578,16 @@ export const processBidderRequests = hook('async', function<B extends BidderCode
         } else {
           callAjax({ url: request.url, payload: typeof request.data === 'string' ? request.data : JSON.stringify(request.data) });
         }
+        break;
+      case 'DIRECT':
+        onSuccess(request.response.responseText, request.response);
+        break;
+      case 'PROMISE':
+        request.promise
+          .then((response) => {
+            onSuccess(response.responseText, response);
+          })
+          .catch((error) => { onFailure(error, null) });
         break;
       default:
         logWarn(`Skipping invalid request from ${spec.code}. Request type ${request.method} must be GET or POST`);

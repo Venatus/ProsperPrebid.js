a4967c2e61514b727283a204605223e4393488a6
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2025-07-05 22:34:11 +0200

    Squashed commit of the following: auctionManager - external bids
    commit 564c82fad2235376416dbec6b20102d8ec256507
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Mon Dec 23 18:02:26 2024 +0100
    
        add bids as item
    
    commit e2767f9bca8584232a910658d7cac7ae5b10c884
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Mon Nov 11 18:55:13 2024 +0100
    
        allow access to all received bids
    
        # Conflicts:
        #	src/prebid.js
    
        # Conflicts:
        #	src/prebid.ts
    
    commit 7e442e9bb3b930c87d8d4905ec9bd8e3414ca9e4
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Mon Apr 15 17:52:15 2024 +0200
    
        basic restoreBids as reference (note, the methods needs to be implemented manually in this case)
    
        # Conflicts:
        #	src/prebid.js
    
    commit d9d3220839816db05633d279f47c785e13f968f0
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Tue Mar 19 15:52:50 2024 +0100
    
        getAuction by Bid instance
    
        # Conflicts:
        #	src/auctionManager.js
    
    commit 18b1ed1c44d85a521ae09a1a24a50e73aaaceb4c
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Thu Aug 3 14:22:05 2023 +0200
    
        basic support to load (external) bids
    
        326afed1d39ca4e3cce002ff3d24023177f32a1d
    
        # Conflicts:
        #	src/auctionManager.js
    
        # Conflicts:
        #	src/auctionManager.js
    
        # Conflicts:
        #	src/auctionManager.js
    
        # Conflicts:
        #	src/adapterManager.js
        #	src/bidfactory.js
        #	src/prebid.js
    
    # Conflicts:
    #	src/adapterManager.ts
    #	src/prebid.ts

--- src/prebid.ts
+++ src/prebid.ts
@@ -485,6 +485,8 @@ declare module './prebidGlobal' {
     refreshPageViewId: typeof refreshPageViewId;
 
     registerBidder: typeof registerBidder;
+    addBids: typeof addBids;
+    getAllReceivedBids: typeof auctionManager.getAllReceivedBids;
   }
 }
 
@@ -978,6 +980,15 @@ function addAdUnits(adUnits: AdUnitDefinition | AdUnitDefinition[]) {
 }
 
 addApiMethod('addAdUnits', addAdUnits);
+
+function addBids(bids, adunit, asReference=false) {
+  auctionManager.addBids(bids, adunit, asReference);
+}
+addApiMethod('addBids', addBids);
+
+pbjsInstance.getAllReceivedBids = function () {
+  return auctionManager.getAllReceivedBids();
+}
 
 const eventIdValidators = {
   bidWon(id) {

377ccf4e040139464a5131ef531f301dab7c0349
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2025-07-05 23:20:41 +0200

    Squashed commit of the following: consent
    commit 8bf4803b04ee22f5cf9b1512dcf64476eb96329c
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Sun Feb 4 20:34:57 2024 +0100
    
        allow infinite consent timeout
    
        # Conflicts:
        #	modules/consentManagementTcf.js
    
        # Conflicts:
        #	libraries/consentManagement/cmUtils.ts
    
    commit 5926537e6acf420205f5a1e55cc1b73954dfdac8
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Thu Dec 14 12:11:22 2023 +0100
    
        don't check for empty consent string

--- modules/consentManagementTcf.ts
+++ modules/consentManagementTcf.ts
@@ -127,7 +127,7 @@ function parseConsentData(consentObject): TCFConsentData {
     const tcString = consentObject && consentObject.tcString;
     return !!(
       (typeof gdprApplies !== 'boolean') ||
-      (gdprApplies === true && (!tcString || !isStr(tcString)))
+      (gdprApplies === true && (/*!tcString ||*/ !isStr(tcString)))
     );
   }
 

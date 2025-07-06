bac4f89137e6cf83266831e89a7d1ab982a9d0be
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2025-07-06 14:38:38 +0200

    Squashed commit of the following: bidder fixes
    commit 7ee69d70725de43170c9b7ac3724ec309a263fc1
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Mon Jun 16 16:11:37 2025 +0200
    
        added support for visibility via bid params
    
    commit 597880e49f520066c534e8edcf4cb9cdee5f10d2
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Tue Oct 22 13:20:26 2024 +0200
    
        allow pubmatic to use a fallback renderer config
    
        # Conflicts:
        #	modules/pubmaticBidAdapter.js
    
    commit 454f3af3bb359f008a85bbedccf0c29e5a96f7d8
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Fri Aug 30 19:56:20 2024 +0200
    
        alias KueezRtb to it's module name
    
    commit 9dd444ec3cbc8382b3e0bacde82b9e1978cecb24
    Author: Olaf Buitelaar <olaf.buitelaar@gmail.com>
    Date:   Wed Jan 17 13:47:11 2024 +0100
    
        don't set ozone in debug mode when logging is enabled
    
    # Conflicts:
    #	modules/ozoneBidAdapter.js
    
    # Conflicts:
    #	modules/pubmaticBidAdapter.js

--- modules/kueezRtbBidAdapter.js
+++ modules/kueezRtbBidAdapter.js
@@ -23,6 +23,9 @@ export const storage = getStorageManager({ bidderCode: BIDDER_CODE });
 
 export const spec = {
   code: BIDDER_CODE,
+  aliases: [
+    {code: 'kueezRtb'},
+  ],
   version: BIDDER_VERSION,
   gvlid: GVLID,
   supportedMediaTypes: [BANNER, VIDEO],
--- modules/ozoneBidAdapter.js
+++ modules/ozoneBidAdapter.js
@@ -319,7 +319,8 @@ export const spec = {
       'page': getRefererInfo().page,
       'id': htmlParams.siteId
     });
-    ozoneRequest.test = config.getConfig('debug') ? 1 : 0;
+    //TODO: make configurable debugable ads -enabled
+    //ozoneRequest.test = config.getConfig('debug') ? 1 : 0;
     if (bidderRequest && bidderRequest.gdprConsent) {
       logInfo('ADDING GDPR');
       const apiVersion = deepAccess(bidderRequest, 'gdprConsent.apiVersion', 1);

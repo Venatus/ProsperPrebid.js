1520f2840f9bc5b2434d266c93d9d7f5c26f121a
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2026-08-13 21:09:01 +0200

    when the BIDDER_INVALID_REQUEST event is emitted pass the original un-proxied/tidGuard-ed bid object

--- src/adapters/bidderFactory.ts
+++ src/adapters/bidderFactory.ts
@@ -308,7 +308,7 @@ export function newBidder<B extends BidderCode>(spec: BidderSpec<B>) {
       }
 
       const validBidRequests = adapterMetrics(bidderRequest)
-        .measureTime('validate', () => bidderRequest.bids.filter((br) => filterAndWarn(tidGuard.bidRequest(br))));
+        .measureTime('validate', () => bidderRequest.bids.filter((br) => filterAndWarn(tidGuard.bidRequest(br), br)));
 
       if (validBidRequests.length === 0) {
         afterAllResponses();
@@ -382,10 +382,10 @@ export function newBidder<B extends BidderCode>(spec: BidderSpec<B>) {
     registerSyncInner(spec, responses, gdprConsent, uspConsent, gppConsent);
   }
 
-  function filterAndWarn(bid) {
+  function filterAndWarn(bid, originialBid?) {
     if (!spec.isBidRequestValid(bid)) {
       logWarn(`Invalid bid sent to bidder ${spec.code}: ${JSON.stringify(bid)}`);
-      events.emit(EVENTS.BIDDER_INVALID_REQUEST, bid);
+      events.emit(EVENTS.BIDDER_INVALID_REQUEST, originialBid ?? bid);
       return false;
     }
     return true;
